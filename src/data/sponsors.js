b8979f8270a15492d66915a9937592fe79a69e7d
sepehr <mr.sabzi.s@gmail.com>
2026-02-27 17:28:00 +0330

    feat: complete responsive Sponsors section

---
+++ src/data/sponsors.js
@@ -0,0 +1,9 @@
+const SponsorLogos = [
+  { id: 1, name: "hooli", img: "/images/fa-brands-1.png" },
+  { id: 2, name: "lya", img: "/images/fa-brands-4.png" },
+  { id: 3, name: "flower", img: "/images/fa-brands-5.png" },
+  { id: 4, name: "stripe", img: "/images/Vector.png" },
+  { id: 5, name: "aws", img: "/images/Vector2.png" },
+  { id: 6, name: "robat", img: "/images/Vector3.png" },
+]
+export default SponsorLogos
